a5ad61674af7d5372fadf1c9f19fae714063df4e
Sarah Vessels <cheshire137@gmail.com>
2010-12-05 19:12:28 -0500

    Fixing bug where not all necessary tables will be joined when a column is in the WHERE clause but not the SELECT clause.  Adding comments. Made table regex in #get_necessary_tables more precise since it's possible to do so since I know the full name of a table.

--- project2/app/models/columnar_distribution.rb
+++ project2/app/models/columnar_distribution.rb
@@ -4,41 +4,55 @@ class ColumnarDistribution < ModelBase
   KeyFieldName = 'id'.freeze
   attr_reader :tables
   
+  # Constructor taking a string table schema.
   def initialize(schema)
     super(schema)
     @tables = ColumnarDistribution.generate_tables(@columns, @data_lines)
   end
   
+  # Creates all tables in the vertical decomposition, using the Ruby classes
+  # generated in the constructor.
   def create_tables
     @tables.each do |table|
       table.create_table
     end
   end
   
+  # Drops all tables in the vertical decomposition, using the Ruby classes
+  # generated in the constructor.
   def drop_tables
     @tables.each do |table|
       table.drop_table
     end
   end
   
+  # Returns an array of table descriptions for display purposes, using the Ruby
+  # classes generated in the constructor.
   def get_tables_descriptions
     @tables.collect do |table|
       table.get_description
     end
   end
   
+  # Returns an array of data from the vertically decomposed tables, using the
+  # Ruby classes generated in the constructor.
   def get_tables_rows_columns
     @tables.collect do |table|
       table.get_data
     end
   end
   
+  # Loads the user-given data into the vertically decomposed tables, using the
+  # Ruby classes generated in the constructor.
   def load_data
     @tables.each do |table|
       table.load_data
     end
   end
   
+  # Runs the given query, translating it as necessary to refer to the vertically
+  # decomposed tables instead of the single table the user expects/submitted.
+  # Returns the results.
   def run_query(query)
     if query.nil? || query.blank?
       raise "Invalid query, cannot be nil or blank"
@@ -47,6 +61,9 @@ class ColumnarDistribution < ModelBase
     query.strip! # Remove trailing whitespace
     query.chomp!(';') # Remove trailing semicolon
     words = query.split # Split on whitespace
+    
+    # Check the query for validity and get the index of the 'FROM' key word in
+    # the array of words in the query.
     from_index = check_query_words(words)
     
     # Even though we're going to ignore the table name they gave, we still
@@ -56,14 +73,31 @@ class ColumnarDistribution < ModelBase
       raise "Invalid query, no table name specified"
     end
     
+    # The rest of the query, skipping 'FROM' and the user-given table name
+    num_words = words.length
+    query_remainder = words[from_index+1...num_words]
+
+    # Get index of 'GROUP' keyword as well as the 'GROUP BY'/'ORDER BY' string
+    # the user gave
+    group_index, group_order_by =
+      ColumnarDistribution.get_group_info(query_remainder)
+    
     # Get Columns from the Columns list in this table, based on the user-given
-    # column names in the query
+    # column names in the query (both SELECT and WHERE clauses)
+    last_where_index = group_index || num_words
     raw_selected_cols = words[1...from_index]
+    raw_where_cols = words[from_index+1...last_where_index]
     selected_cols = get_columns_by_name(raw_selected_cols)
-    tables_used = get_necessary_tables(selected_cols)
-    query_remainder = words[from_index+2...words.length]
+    where_cols = get_columns_by_name(raw_where_cols)
+    
+    # Get an array of tables this query references, for use in constructing
+    # a new query to get the user-requested data
+    tables_used = get_necessary_tables(selected_cols + where_cols)
+    
+    # The WHERE block, including the necessary table joins and any WHERE,
+    # GROUP BY, and ORDER BY the user may have included
     where_block = ColumnarDistribution.get_where_block(tables_used,
-      query_remainder)
+      query_remainder, group_index, group_order_by)
 
     # Everything checks out, so construct a query that uses the vertically
     # partitioned tables to get the user-requested data
@@ -86,20 +120,19 @@ FROM %s
   end
   
   private
+    # Returns a string of the columns the user selected in their query,
+    # wrapped in function calls as necessary.
     def get_columns_block(columns)
       columns.collect do |column|
-        col_name = column.name
         col_func = column.function
         if col_func.nil?
           func_start = ''
           func_end = ''
-          selected_name = col_name
         else
           func_start = col_func + '('
           func_end = ')'
-          selected_name = sprintf("%s(%s)", col_func, col_name)
         end
-        func_start + col_name + func_end
+        func_start + column.name + func_end
       end.join(",\n       ")
     end
   
@@ -124,11 +157,15 @@ FROM %s
       from_index
     end
     
-    # Thanks to http://snippets.dzone.com/posts/show/2390
+    # Thanks to http://snippets.dzone.com/posts/show/2390.  This involves some
+    # Ruby metaprogramming to create a new class at runtime.
     def self.create_class(class_name, superclass, &block)
       Object.const_set(class_name, Class.new(superclass, &block))
     end
 
+    # Creates Ruby classes to interact with tables from the vertical
+    # decomposition of the user's schema.  Returns an array of instantiations of
+    # the new classes.
     def self.generate_tables(columns, data)
       if columns.nil? || columns.empty?
         raise 'No columns given for vertical partitioning'
@@ -141,11 +178,21 @@ FROM %s
       get_models(table_schemas)
     end
     
+    # Creates Ruby classes for each of the tables represented in the given array
+    # of table schemas.  Each Ruby class acts as an interface for creating the
+    # table, dropping the table, etc.
     def self.get_models(schemas)
       return [] if schemas.nil?
       schemas.collect do |table_hash|
         table_name = table_hash[:name]
+        
+        # Turn a string like id_lname into IdLname, which is a suitable Ruby
+        # class name.
         class_name = table_name.camelize
+        
+        # Ruby metaprogramming!  This creates a class whose base class is
+        # ModelBase, whose name is the class_name variable, and that has
+        # the methods given in the do...end block below.
         new_class = create_class(class_name, ModelBase) do
           def initialize(schema, table_name)
             super(schema, table_name)
@@ -180,17 +227,30 @@ FROM %s
             ModelBase.drop_table(@table_name)
           end
         end
+        
+        # Create an instance of the new class, giving it its table schema and
+        # the name of the table.
         new_class.new(table_hash[:schema], table_name)
       end
     end
     
+    # Returns an array of Ruby classes for interacting with the tables
+    # necessary to complete the user-given query.  The array of necessary
+    # table classes is determined based on the given array of columns the user
+    # SELECTed in their query.
     def get_necessary_tables(selected_cols)
       if selected_cols.nil? || selected_cols.empty?
         raise "Invalid query, no columns were SELECTed"
       end
       selected_cols.collect do |column|
-        table_regex = /_#{column.name}$/
+        # Find the first decomposed table whose name matches the pattern
+        # id_columnName, and that will be the relevant table for the current
+        # column.
+        table_regex = /^#{KeyFieldName}_#{column.name}$/
         table = @tables.find { |tbl| !(tbl.table_name =~ table_regex).nil? }
+        
+        # If we did not find such a table, the user gave an invalid query,
+        # SELECTing a column that did not exist in their schema.
         if table.nil?
           raise "Invalid query: #{column.name} is not a valid column"
         end
@@ -223,7 +283,8 @@ FROM %s
       Matrix.rows(column_values).transpose.to_a
     end
     
-    def self.get_where_block(tables, query_remainder)
+    def self.get_where_block(tables, query_remainder, group_index,
+      group_order_by)
       table_names_keys = tables.collect do |table|
         sprintf("%s.%s", table.table_name, KeyFieldName)
       end
@@ -246,14 +307,15 @@ FROM %s
       
       query_length = query_remainder.length;
       lowercase_query = query_remainder.map(&:downcase)
-      group_index, group_order_by = get_group_info(lowercase_query,
-        query_remainder, query_length)
       where_clause = get_user_where_clause(lowercase_query, query_remainder,
         query_length, table_join, group_index)
       sprintf("WHERE %s\n%s", where_clause, group_order_by)
     end
     
-    def self.get_group_info(lowercase_query, query_remainder, query_length)
+    def self.get_group_info(query_remainder)
+      return [nil, ''] if query_remainder.nil? || query_remainder.empty?
+      lowercase_query = query_remainder.map(&:downcase)
+      query_length = query_remainder.length
       group_index = lowercase_query.index { |word| 'group' == word }
       if group_index.nil?
         group_order_by = ''
